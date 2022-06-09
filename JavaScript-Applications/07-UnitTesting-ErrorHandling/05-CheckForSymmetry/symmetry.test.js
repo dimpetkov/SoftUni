39b1f59e3d653a23d7ed2e82de9b3a1cff2464bd
Dimitar Petkov <dim.petkov.dp@gmail.com>
2022-06-09 12:59:28 +0200

    Add files via upload

---
+++ JavaScript-Applications/07-UnitTesting-ErrorHandling/05-CheckForSymmetry/symmetry.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const { isSymmetric } = require('./symmetry');
+
+describe('Symmetry Checker', () => {
+    it('works with symmetric numeric array', () => {
+        expect(isSymmetric([1, 2, 2, 1])).to.be.true;
+    });
+    it('returns false for non-symmetric numeric array', () => {
+        expect(isSymmetric([1, 2, 3])).to.be.false;
+    });
+    it('returns false for non-array', () => {
+        expect(isSymmetric(5)).to.be.false;
+    });
+    it('works with symmetric odd-length array', () => {
+        expect(isSymmetric([1, 2, 1])).to.be.true;
+    });
+    it('works with symmetric string array', () => {
+        expect(isSymmetric(['a', 'b', 'b', 'a'])).to.be.true;
+    });
+    it('return false for string param', () => {
+        expect(isSymmetric('abba')).to.be.false;
+    });
+    it('returns false for type mismatched element', () => {
+        expect(isSymmetric([1, 2, '1'])).to.be.false;
+    });
+
+});
